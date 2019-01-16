548577369befb4482eadbad6e83eed3c9b722963
Cait Sidener <cait.sidener@gmail.com>
2019-01-16 11:52:20 -0600

    clean up

--- assets/js/logic.js
+++
@@ -1,9 +0,0 @@
-var player = new Spotify.Player({
-    name: 'Carly Rae Jepsen Player',
-    getOAuthToken: callback => {
-      // Run code to get a fresh access token
-  
-      callback('access token here');
-    },
-    volume: 0.5
-  });
