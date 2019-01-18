51c2e9a71718d0dc2663470aa83657b5ba75d5d0
Perla Ordonez <guate.po@gmail.com>
2019-01-17 21:08:32 -0600

    cleaned up code for commute time logic

--- assets/js/commute-time.js
+++ assets/js/commute-time.js
@@ -1,19 +1,31 @@
-// function that uses Location API to turn address into longtitude and latitude coordinates
-var startPoint="127 Topping Ave, Kansas City MO 64123";
-var startRes = encodeURI(startPoint);
-console.log(startRes);
-var endPoint = "3902 Main St, Kansas City MO 64111";
-var endRes = encodeURI(endPoint);
-console.log(endRes);
 var key ="Ak1mPSqAHW1l1MRNPcHfPyf5W6Awjxu4iGI-YqF_d7yb0LE5Ik5opIazkb8glEAQ";
+function geoLocation(origin, destination){
+        var startRes = encodeURI(origin);
+        var endRes = encodeURI(destination)
+    var queryURL = "http://dev.virtualearth.net/REST/V1/Routes/Driving?wp.0=" + startRes + "&wp.1=" + endRes + "&optimize=timewithTraffic&key=" + key;  
 
-var location = function(geoLocation){
-    var queryURL = "http://dev.virtualearth.net/REST/v1/Locations?q=" + startRes + "&key=" + key;
     $.ajax({
         url:queryURL,
         method: "GET"
     }).then(function(response){
-        console.log(response);
+        var totalSeconds = response.resourceSets[0].resources[0].travelDurationTraffic;
+        console.log(totalSeconds);
+        var hours = Math.floor(totalSeconds / 3600);
+            console.log(hours);
+        var minutes = Math.floor(totalSeconds / 60);
+        console.log(minutes);
+        var seconds = totalSeconds % 60;
+        console.log(seconds);
+        $("#display").text("Total Commute time " + hours+ ":"+ minutes+ ":"+ seconds);
     });
-    geoLocation();
-};
+}
+    //gets origin and destination input from user
+    $("#submit").on("click", function(event){
+        event.preventDefault();
+    
+        var origin = $("#startPoint").val().trim();
+        var destination = $("#endPoint").val().trim();
+        console.log("Origin: ", origin, "Destination: ", destination);
+        geoLocation(origin, destination);
+    });
+    
