37aaa781a06b96d614d4aed3fcd169754b15c1af
Cait Sidener <cait.sidener@gmail.com>
2019-01-17 18:47:41 -0600

    headlines

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
+  // queryURL is the url we'll use to query the API
+  var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
 
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    // After data comes back from the request
+    .then(function(response) {
+      console.log(queryURL);
+      console.log(response);
 
-    
 })
