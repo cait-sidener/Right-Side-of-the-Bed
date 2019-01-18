0e05e5f108387aeb5630f0c93e9d309008f599ba
Cait Sidener <cait.sidener@gmail.com>
2019-01-17 19:56:46 -0600

    on click listener failure

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,17 +1,39 @@
-$(document).ready(function(){
-  // queryURL is the url we'll use to query the API
-  var queryURL = "https://api.nytimes.com/svc/topstories/v2/home.json?api-key=5G5trsY71XEGyCzsg97O2rky6AnWpUOt";
-  
+$("#headline-news").on("click", function () {
 
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  })
-    // After data comes back from the request
-    .then(function(response) {
-      console.log(response);
+    // queryURL is the url we'll use to query the API
+    var queryURL = "https://api.nytimes.com/svc/topstories/v2/home.json?api-key=5G5trsY71XEGyCzsg97O2rky6AnWpUOt";
 
-      //Creating a div to hold the movie
 
-})
+    $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        // After data comes back from the request
+        .then(function (response) {
+            console.log(response);
+
+            //Creating a div to hold the movie
+            var newsDiv = $("<div class='news'>");
+
+            // Storing headline abstract data
+            var headline = response.results.abstract;
+
+            // Creating an element to have headline abstract display
+            var abstract = $("<h3>").text(headline);
+
+            // Displaying the abstract
+            newsDiv.append(abstract);
+
+            // Storing the source
+            var source = response.results.url;
+
+            // Creating an element to have headline url displayed
+            var newsSource = $("<p>").text(source);
+
+            // Displaying the URL
+            newsDiv.append(newsSource);
+
+            // Putting the entire headline above the previous headlines
+            $("#headline-view").prepend(newsDiv);
+        })
 })
